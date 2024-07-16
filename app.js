92efc3c90517159fbbe8a504e6b0a24f92e20aef
mhuzaifa360 <mhuzaifa3128352877@gmail.com>
2024-07-16 16:00:42 +0500

    solve all 5 progrms of javaScript and push

---
+++ app.js
@@ -0,0 +1,33 @@
+//!-- Exercise 1
+//?-- Task: Declare a variable str and assign it the value "Welcome to JavaScript!". Print the length of the string.
+
+// let str = "Wellcome to JavaScript!"
+// console.log("Length of str is : " + str.length);
+
+//!-- Exercise 2
+//?-- Task: Declare a variable str and assign it the value "Programming is awesome". Extract and print the word "awesome" from the string.
+
+// var str = "Programming is awesome";
+// var extractElement = str.slice(15);
+// console.log({extractElement});
+
+//!--  Exercise 3
+//?-- Task: Declare a variable str and assign it the value "Hello, Universe!". Extract and print the first five characters of the string.
+
+// var str = "Hello, Universe!";
+// var extractElement = str.slice(0, 5);
+// console.log({extractElement});
+
+//!-- Exercise 4
+//?-- Task: Declare a variable str and assign it the value "Full Stack Developer". Extract and print the substring "Stack".
+
+// var str = "Full Stack Developer";
+// var extractElement = str.slice(5, 10);
+// console.log({extractElement});
+
+//!-- Exercise 5
+//?-- Task: Declare a variable str and assign it the value "Javascript Strings" Extract and print the last four characters of the string.
+
+// var str = "JavaScript Strings";
+// var extractElement = str.slice(14);
+// console.log({extractElement});
